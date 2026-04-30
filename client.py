cff0a9d81539e476e44f3029d5ae9009636d5a61
Temper123529 <yangtenbo20060610@qq.com>
2026-04-30 19:58:32 +0800

    feat: initial code

---
+++ client.py
@@ -0,0 +1,4 @@
+# client.py
+# COMPX234-A3 Tuple Space Client
+if __name__ == "__main__":
+    print("Client starting...")
---
+++ server.py
@@ -0,0 +1,4 @@
+# server.py
+# COMPX234-A3 Tuple Space Server
+if __name__ == "__main__":
+    print("Server starting...")
