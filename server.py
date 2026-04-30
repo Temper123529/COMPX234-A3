26a90b063a72221f9c49ae0a2eeaea9905687f10
Temper123529 <yangtenbo20060610@qq.com>
2026-04-30 20:01:29 +0800

    feat: add thread-safe tuple space

--- server.py
+++ server.py
@@ -1,4 +1,19 @@
-# server.py
-# COMPX234-A3 Tuple Space Server
-if __name__ == "__main__":
-    print("Server starting...")
+import threading
+class TupleSpace:
+    def __init__(self):
+        self.data = {}
+        self.lock = threading.Lock()
+    def put(self, k, v):
+        with self.lock:
+            if k not in self.data:
+                self.data[k] = v
+                return True
+            return False
+    def get(self, k):
+        with self.lock:
+            return self.data.pop(k, None)
+    def read(self, k):
+        with self.lock:
+            return self.data.get(k, None)
+if __name__ == '__main__':
+    print("TupleSpace Loaded")
